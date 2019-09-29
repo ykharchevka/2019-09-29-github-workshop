3e31cbd3bdcf3211693f6c3877edf2a86a49b18b
Yehor <kharchevka@gmail.com>
2019-09-29 18:17:32 +0300

    changed main py

--- github_bot/__main__.py
+++ github_bot/__main__.py
@@ -1,4 +1,21 @@
+from octomachinery.app.routing import process_event_actions
+from octomachinery.app.routing.decorators import process_webhook_payload
+from octomachinery.app.runtime.context import RUNTIME_CONTEXT
 from octomachinery.app.server.runner import run as run_app
+
+@process_event_actions('issues', {'opened'})
+@process_webhook_payload
+async def on_issue_opened(
+        *,
+        action, issue, repository, sender, installation,
+        assignee=None, changes=None,
+):
+    """Whenever an issue is opened, greet the author and say thanks."""
+    github_api = RUNTIME_CONTEXT.app_installation_client
+    comments_api_url = issue['comments_url']
+    #
+    # await github_api.post(url, data={"body": message})
+    # url = f"/repos/mariatta/strange-relationship/issues/{issue_number}/comments"
 
 
 if __name__ == "__main__":
